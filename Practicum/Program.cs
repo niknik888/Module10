bd876c066cfff04e3d3d5354dfe889d5efdfa311
niknik888 <nv.nikolaev@mail.ru>
2023-09-24 17:44:56 +0300

    Выполнен практикум

---
+++ Practicum/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+
+
+namespace Practicum
+{
+    public class Program
+    {
+        ILogger Logger { get; set; }
+        public static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate(new Logger());
+            try
+            {
+                Console.Write("Введите первое число: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Результат: {calculate.Action(a, b)}");
+            }
+            catch (FormatException)
+            {
+                calculate.ActionError("Ошибка: Введите корректное числовое значение.");
+            }
+            catch (Exception ex)
+            {
+                calculate.ActionError($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+
+    }
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+
+
+
+    public interface ICalculate
+    {
+        public int Action(int a, int b);
+        public void ActionError(string message);
+    }
+
+
+    public class Calculate : ICalculate
+    {
+        private readonly ILogger _logger;
+
+        public Calculate(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int Action(int a, int b)
+        {
+            _logger.Event("Выполнена операция сложения.");
+            return a + b;
+        }
+
+        public void ActionError(string message)
+        {
+            _logger.Error(message);
+        }
+    }
+}
---
+++ Task1044/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+public class Program
+{
+    public void Main()
+    {
+        var user = new User();
+        var account = new Account();
+
+        IUpdater<Account> updater = new UserService();
+
+        var userService = new UserService();
+        userService.Update(user);
+    }
+
+}
+
+public class User
+{
+
+}
+
+public class Account : User
+{
+
+}
+
+
+public interface IUpdater<in T>
+{
+    void Update(T entity);
+}
+
+public class UserService : IUpdater<User>
+{
+    public void Update(User entity)
+    {
+        throw new NotImplementedException();
+    }
+}
